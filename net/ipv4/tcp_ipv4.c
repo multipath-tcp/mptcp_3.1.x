2fa4b5d15282544ffcb318c636fabb3a16dce8b8
Mihai P. Andrei <mihai.p.andrei@intel.com>
2012-08-31 16:52:26 +0300

    mptcp: Minor coding style fixes
    Minor coding style fixes for the patch "Don't call mptcp_lookup_join on every SYN for IPv4".
    
    Signed-off-by: Mihai P. Andrei <mihai.p.andrei@intel.com>
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1324,6 +1324,7 @@ int tcp_v4_conn_request(struct sock *sk, struct sk_buff *skb)
 #ifdef CONFIG_MPTCP
 	if (tmp_opt.saw_mpc && mopt.is_mp_join) {
 		int ret;
+
 		ret = mptcp_do_join_short(skb, &mopt, &tmp_opt);
 		if (ret < 0) {
 			tcp_v4_send_reset(NULL, skb);
@@ -1789,6 +1790,7 @@ process:
 #ifdef CONFIG_MPTCP
 	if (!sk && th->syn && !th->ack) {
 		int ret;
+
 		ret = mptcp_lookup_join(skb);
 		if (ret) {
 			if (ret < 0) {
@@ -1919,6 +1921,7 @@ do_time_wait:
 #ifdef CONFIG_MPTCP
 		if (th->syn && !th->ack) {
 			int ret;
+
 			ret = mptcp_lookup_join(skb);
 			if (ret) {
 				/* As we come from do_time_wait, we are sure that
